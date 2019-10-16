02ed78039c569f6d6b7b3c609014904929336656
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-10-15 23:32:24 -0300

    first commit

--- config/config.php
+++ config/config.php
@@ -10,4 +10,4 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', 'http://localhost/nfe/');
+define('URL_BASE', 'http://developer.oursys.com.br/nfe/');
