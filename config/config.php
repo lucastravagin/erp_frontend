9fd2ad862100c73a974fa44bbd7664527e2d59a5
Lucas Travagin <lucas_travagin1112@hotmail.com>
2019-10-16 00:48:59 -0300

    first commit

--- config/config.php
+++ config/config.php
@@ -10,4 +10,4 @@ define('CONTROLLER_PADRAO', 'home');
 define('METODO_PADRAO', 'index');
 define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
 
-define('URL_BASE', 'http://developer.oursys.com.br/');
+define('URL_BASE', 'https://developer.oursys.com.br/');
